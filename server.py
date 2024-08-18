724b0d72e444871fc14143f008eb1a0f60b415e6
Adnan-ibrahim1 <66627818+Adnan-ibrahim1@users.noreply.github.com>
2024-08-18 23:20:57 +0200

    attempt 1

--- server.py
+++ server.py
@@ -1,27 +1,14 @@
-from flask import *
+from flask import Flask, render_template
 import getCryptoData as gcd
 import getStockData as gsd
 
-trackerApp= Flask(__name__)
+trackerApp = Flask(__name__)
 
 @trackerApp.route("/")
 def dataPresent():
-    return render_template("")
-# should be the front page
-# has two boxes (stocks / crypto) in the middle
-# 
+    stockInfo = gsd.startCollectStock()
+    cryptoInfo = gcd.startCollectCrypto()
+    return render_template("index.html", stockInfo=stockInfo, cryptoInfo=cryptoInfo)
 
-# @trackerApp.route("/stocks")
-# def stocksPresent():
-#     stockInfo= gsd.startCollectStock()
-#     return render_template("", stockInfo= stockInfo)
-#     pass
-
-# @trackerApp.route("/crypto")
-# def cryptoPresent():
-#     cryptoInfo= gcd.startCollectCrypto()
-#     return render_template("", cryptoInfo= cryptoInfo)
-#     pass
-    
 if __name__ == "__main__":
-    trackerApp.run(debug= False)
+    trackerApp.run(debug=False)
