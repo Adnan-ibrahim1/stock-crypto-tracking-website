020ed9a4dd74585b036a510d71a6ebe7b89a9379
George Veselinoff <159577656+neihon@users.noreply.github.com>
2024-08-20 22:19:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def dataPresent():
-    # Collect stock and crypto data
-    stockInfo = startCollectStock()  # This should return a list of stockUnit objects
-    cryptoInfo = startCollectCrypto()  # This should return a list of cryptoUnit objects
+    
+    stockInfo = startCollectStock()
+    cryptoInfo = startCollectCrypto()
 
-    # Render the template with the data
     return render_template("index.html", stockInfo=stockInfo, cryptoInfo=cryptoInfo)
 
 
