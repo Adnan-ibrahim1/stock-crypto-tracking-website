68feca8b4a8f84ad732f222ce7cac4baabea2344
neihon <jorogeorgievxd@gmail.com>
2024-08-20 22:05:55 +0300

    Add Plots for the Price of Each Stock over the Course of 1 Month

---
+++ dataVisualise.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import yfinance as yf
+import getStockData as gsd
+from datetime import datetime
+
+def getData():
+    stockList= gsd.startCollectStock()
+
+    tickers= []
+    for stock in stockList:
+        tickers.append(stock.ticker)
+    
+    return plotData(tickers)
+
+def plotData(data):
+    plotList= []
+
+    for element in data:
+        all_data= yf.Ticker(f"{element}")
+        hist= all_data.history(period= "1mo", interval= "1d")
+        histClose= hist["Close"]
+        dates= hist.index
+        
+        plotList.append(createPlot(element, dates, histClose))
+    
+    return plotList
+    
+def createPlot(stock, dates, closePrice):
+   
+    plot= plt.figure(figsize= (10,5))
+    plt.plot(dates, closePrice, marker= "o", linestyle= "-", color= "green")
+    plt.title(f"{stock}")
+    plt.xlabel("Date")
+    plt.ylabel("Closing Price")
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+    
+    return plot
